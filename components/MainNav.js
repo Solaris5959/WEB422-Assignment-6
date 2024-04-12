6228d7e51058218c8852cea34b844fd477188f5c
Connor McDonald <connormmcdonald@gmail.com>
2024-04-12 19:40:23 -0400

    Added navigation protection against non-users, updated main nav for authorization states

--- components/MainNav.js
+++ components/MainNav.js
@@ -5,11 +5,18 @@ import Link from 'next/link';
 import { useAtom } from 'jotai';
 import { searchHistoryAtom } from '@/store.js'; // Ensure correct path
 import { addToHistory } from '@/lib/userData'; // Ensure this path is correct
+import { readToken, removeToken } from '@/lib/authenticate';
 
 export default function MainNav() {
     const router = useRouter();
     const [isExpanded, setIsExpanded] = useState(false);
     const [searchHistory, setSearchHistory] = useAtom(searchHistoryAtom);
+    let token = readToken();
+
+    function logout() {
+        removeToken();
+        router.push('/');
+    }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -42,26 +49,41 @@ export default function MainNav() {
                             <Link href="/" legacyBehavior passHref>
                                 <Nav.Link active={router.pathname === "/"} onClick={handleLinkClick}>Home</Nav.Link>
                             </Link>
-                            <Link href="/search" legacyBehavior passHref>
-                                <Nav.Link active={router.pathname === "/search"} onClick={handleLinkClick}>Advanced Search</Nav.Link>
-                            </Link>
-                        </Nav>
-                        &nbsp;
-                        <Form className="d-flex" onSubmit={handleSubmit}>
-                            <FormControl type="text" name="search" placeholder="Search" className="me-2" />
-                            <Button className="btn btn-success" type="submit">Search</Button>
-                        </Form>
-                        &nbsp;
-                        <Nav>
-                            <NavDropdown title="User Name" id="nav-dropdown">
-                                <Link href="/favourites" legacyBehavior passHref>
-                                    <NavDropdown.Item active={router.pathname === "/favourites"}>Favourites</NavDropdown.Item>
+                            {token && (
+                                <Link href="/search" legacyBehavior passHref>
+                                    <Nav.Link active={router.pathname === "/search"} onClick={handleLinkClick}>Advanced Search</Nav.Link>
                                 </Link>
-                                <Link href="/history" legacyBehavior passHref>
-                                    <NavDropdown.Item active={router.pathname === "/history"}>Search History</NavDropdown.Item>
-                                </Link>
-                            </NavDropdown>
+                            )}
                         </Nav>
+                        {token ? (
+                            <>
+                                <Form className="d-flex" onSubmit={handleSubmit}>
+                                    <FormControl type="text" name="search" placeholder="Search" className="me-2" />
+                                    <Button className="btn btn-success" type="submit">Search</Button>
+                                </Form>
+                                &nbsp;
+                                <Nav>
+                                    <NavDropdown title={token.userName || "User Name"} id="nav-dropdown">
+                                        <Link href="/favourites" legacyBehavior passHref>
+                                            <NavDropdown.Item onClick={handleLinkClick}>Favourites</NavDropdown.Item>
+                                        </Link>
+                                        <Link href="/history" legacyBehavior passHref>
+                                            <NavDropdown.Item onClick={handleLinkClick}>Search History</NavDropdown.Item>
+                                        </Link>
+                                        <NavDropdown.Item onClick={logout}>Logout</NavDropdown.Item>
+                                    </NavDropdown>
+                                </Nav>
+                            </>
+                        ) : (
+                            <Nav>
+                                <Link href="/login" legacyBehavior passHref>
+                                    <Nav.Link active={router.pathname === "/login"} onClick={handleLinkClick}>Login</Nav.Link>
+                                </Link>
+                                <Link href="/register" legacyBehavior passHref>
+                                    <Nav.Link active={router.pathname === "/register"} onClick={handleLinkClick}>Register</Nav.Link>
+                                </Link>
+                            </Nav>
+                        )}
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
---
+++ components/RouteGuard.js
@@ -0,0 +1,43 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useAtom } from 'jotai';
+import { favouritesAtom, searchHistoryAtom } from '@/store';
+import { getFavourites, getHistory } from '@/lib/userData';
+
+const PUBLIC_PATHS = ['/login', '/', '/_error', '/register'];
+
+export default function RouteGuard(props) {
+    const router = useRouter();
+    const [favouritesList, setFavouritesList] = useAtom(favouritesAtom);
+    const [searchHistory, setSearchHistory] = useAtom(searchHistoryAtom);
+
+    async function updateAtoms() {
+        setFavouritesList(await getFavourites());
+        setSearchHistory(await getHistory());
+    }
+
+    useEffect(() => {
+        updateAtoms();  // Update atoms on initial load
+
+        const handleRouteChange = (url) => {
+            authCheck(url);
+            updateAtoms();
+        };
+
+        router.events.on('routeChangeComplete', handleRouteChange);
+
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange);
+        };
+    }, []);
+
+    function authCheck(url) {
+        const path = url.split('?')[0];
+        if (!PUBLIC_PATHS.includes(path) && !localStorage.getItem('token')) {
+            console.log(`Redirecting to login because the path ${path} is protected.`);
+            router.push('/login');
+        }
+    }
+
+    return <>{props.children}</>
+}
