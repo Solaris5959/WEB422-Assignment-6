f9fb08024f9cf90dd5a5f676e3db2c0e600afccc
Connor McDonald <connormmcdonald@gmail.com>
2024-04-12 20:09:42 -0400

    update index for a6 reqs

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 /*********************************************************************************
-*  WEB422 – Assignment 5
+*  WEB422 – Assignment 6
 *
 *  I declare that this assignment is my own work in accordance with Seneca's
 *  Academic Integrity Policy:
@@ -7,6 +7,8 @@
 *  https://www.senecapolytechnic.ca/about/policies/academic-integrity-policy.html
 * 
 *  Name: Connor McDonald Student ID: 136123221 Date: Feb 23, 2024
+*
+*  Vercel App (Deployed) Link: https://web422-assignment-6.vercel.app/
 *
 ********************************************************************************/
 
